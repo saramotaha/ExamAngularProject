b471fbc12d90d69c989f866bf6af530bcd2a6150
saramotaha <saramohamedtaha909@gmail.com>
2025-07-11 18:05:42 +0300

    account

---
+++ ExamSystem/DTOs/UserRegisterDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExamSystem.DTO
+{
+    public class UserRegisterDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PassWord { get; set; }
+
+        [Compare("PassWord")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string role { get; set; }
+
+
+
+
+    }
+}
