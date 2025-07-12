c2c79461af8a73a7b73e819db6a577aa337e0099
saramotaha <saramohamedtaha909@gmail.com>
2025-07-12 13:21:11 +0300

    ExamController

---
+++ ExamSystem/DTOs/ExamDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExamSystem.DTOs
+{
+    public class ExamDto
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        [Required]
+
+        public int Duration { get; set; }
+        [Required]
+
+        public int TotalScore { get; set; }
+
+
+        
+        //public int UsersId { get; set; }
+    }
+}
