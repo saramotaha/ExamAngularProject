4931a65df6437b1e5a2214659ff68d613ab4e2d6
rawan_Bakr <rawan95bakr@gmail.com>
2025-07-13 03:24:36 +0300

    FinishQuestion

--- ExamSystem/Repositories/Implementation/QuestionRepo.cs
+++ ExamSystem/Repositories/Implementation/QuestionRepo.cs
@@ -9,27 +9,49 @@ namespace ExamSystem.Repositories.Implementation;
 
 public class QuestionRepo : Repository<Question>, IQuestionRepo
 {
-    private readonly ExamContext _questions;
+    private readonly ExamContext _context;
 
     public QuestionRepo(ExamContext context) : base(context)
     {
-        _questions = context;
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Question>> GetAllQuestionsByExamIdAsync(int examId)
+    {
+       return await _context.Questions
+            .Where(q => q.ExamID == examId)
+            .Include(q => q.Choices)
+            .ToListAsync();
     }
 
     public async Task<Question?> GetByIdWithAnswersAsync(int id)
     {
-        return await _questions.Questions
+        return await _context.Questions
             .Include(q => q.Choices)
             .FirstOrDefaultAsync(q => q.ID == id);
     }
 
-    //Task<QuestionDto?> IQuestionRepo.GetByIdWithAnswersAsync(int id)
-    //{
-    //    throw new NotImplementedException();
-    //}
+    public async Task<Question?> GetQuestionByIdAsync(int id)
+    {
+        return await _context.Questions
+            .FirstOrDefaultAsync(q => q.ID == id);
+    }
 
-    //Task<QuestionDto?> IQuestionRepo.GetByIdWithAnswersAsync(int id)
-    //{
-    //    throw new NotImplementedException();
-    //}
+    public async Task DeleteAnswersAsync(IEnumerable<Answers> answers)
+    {
+        _context.Answers.RemoveRange(answers);
+    }
+
+    public async Task DeleteQuestionAsync(Question question)
+    {
+        _context.Questions.Remove(question);
+    }
+
+    public async Task<int> CountByExamIdAsync(int examId)
+    {
+        return await _context.Questions
+                             .Where(q => q.ExamID == examId)
+                             .CountAsync();
+    }
+
 }
